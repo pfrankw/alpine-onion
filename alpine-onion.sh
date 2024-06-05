0aa919ae17d7f15ea15938833df35ee9483f4006
Francesco Pompò <pfrankw@users.noreply.github.com>
2024-06-05 18:13:24 +0200

    Added DHCP server

--- alpine-onion.sh
+++ alpine-onion.sh
@@ -15,10 +15,11 @@ do
 done > /etc/apk/repositories
 
 apk update
-apk add tor iptables nyx
+apk add tor iptables nyx dnsmasq
 
 echo -e "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet dhcp\n\nauto eth1\niface eth1 inet static\n\taddress 10.152.152.10\n\tnetmask 255.255.255.0" > /etc/network/interfaces
 echo -e "VirtualAddrNetworkIPv4 10.192.0.0/10\nAutomapHostsOnResolve 1\nTransPort 10.152.152.10:9040\nDNSPort 10.152.152.10:5353\nControlPort unix:/var/run/tor/control RelaxDirModeCheck" > /etc/tor/torrc
+echo -e "port=0\ndhcp-range=10.152.152.11,10.152.152.200,12h\ndhcp-option=option:router,10.152.152.10\ndhcp-option=option:dns-server,10.152.152.10\ndhcp-lease-max=150\ndhcp-leasefile=/var/lib/misc/dnsmasq.leases" > /etc/dnsmasq.conf
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/ip_forward.conf
 
 iptables -F
@@ -35,11 +36,14 @@ iptables -A OUTPUT -m owner --uid-owner $(id -u tor) -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -d 10.152.152.10 -p tcp --dport 9040 -j ACCEPT
 iptables -A INPUT -d 10.152.152.10 -p udp --dport 5353 -j ACCEPT
+iptables -A INPUT -d 10.152.152.10 -p udp --dport 67 -j ACCEPT
 
 rc-update add iptables
 rc-service iptables save
 
 rc-update add tor
 rc-update del syslog boot
+
+rc-update add dnsmasq
 
 echo "Installation complete. Please reboot."
