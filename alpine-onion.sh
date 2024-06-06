07d9e5b1b8085c4af966f250da5df0e2c491a69c
Francesco Pompò <pfrankw@users.noreply.github.com>
2024-06-06 11:00:57 +0200

    Typo fix

--- alpine-onion.sh
+++ alpine-onion.sh
@@ -15,7 +15,7 @@ do
 done > /etc/apk/repositories
 
 apk update
-apk add tor iptables nyx dhcpd
+apk add tor iptables nyx dhcp
 
 echo -e "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet dhcp\n\nauto eth1\niface eth1 inet static\n\taddress 10.152.152.10\n\tnetmask 255.255.255.0" > /etc/network/interfaces
 echo -e "VirtualAddrNetworkIPv4 10.192.0.0/10\nAutomapHostsOnResolve 1\nTransPort 10.152.152.10:9040\nDNSPort 10.152.152.10:5353\nControlPort unix:/var/run/tor/control RelaxDirModeCheck" > /etc/tor/torrc
